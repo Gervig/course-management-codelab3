95f07c1908e3e61a1ea2fb4ea188fb81f153cb87
RikkeMariussen <cph-rm225@cphbusiness.dk>
2025-02-12 09:55:04 +0100

    Startet på attributter og constructor

--- src/main/java/app/entities/Teacher.java
+++ src/main/java/app/entities/Teacher.java
@@ -1,5 +1,36 @@
 package app.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@DynamicUpdate
+
 public class Teacher
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String email;
+    private String name;
+    private String zoom;
+
+    public Teacher(String email, String name, String zoom)
+    {
+        this.email = email;
+        this.name = name;
+        this.zoom = zoom;
+    }
+
+
+    //Relationer
 }
