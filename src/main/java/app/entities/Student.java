2731c4223fb678e50b2cef4c5822e3b30ad43b50
RikkeMariussen <cph-rm225@cphbusiness.dk>
2025-02-12 10:00:58 +0100

    Lavet attributter og constructor og prepersist til tid

--- src/main/java/app/entities/Student.java
+++ src/main/java/app/entities/Student.java
@@ -1,5 +1,45 @@
 package app.entities;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@DynamicUpdate
+
 public class Student
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+    private String email;
+    private String name;
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
+
+    public Student(LocalDateTime createdAt, String email, String name, LocalDateTime updatedAt)
+    {
+        this.createdAt = createdAt;
+        this.email = email;
+        this.name = name;
+        this.updatedAt = updatedAt;
+    }
+
+    //"Stempler" eleven med localDateTime
+    @PrePersist
+    private void addCreated(){
+        createdAt = LocalDateTime.now();
+    }
+
 }
